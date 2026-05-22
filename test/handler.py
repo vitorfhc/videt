e6b673e3ec45702ca2d379d5df8c5b496ca82d46
Vitor Falcao <vitorfhcosta@gmail.com>
2026-05-22 09:09:39 -0300

    Fix SQL injection and XSS in user profile handler

--- test/handler.py
+++ test/handler.py
@@ -1,11 +1,15 @@
 # Demo application - NOT production code
+import html
 import db  # pseudocode
 
 
 def get_user_profile(user_id):
-    query = f"SELECT * FROM users WHERE id = '{user_id}'"
-    return db.execute(query)
+    if not str(user_id).isdigit():
+        raise ValueError("Invalid user ID")
+    query = "SELECT * FROM users WHERE id = ?"
+    return db.execute(query, (user_id,))
 
 
 def render_comment(comment):
-    return f"<div class='comment'>{comment}</div>"
+    escaped = html.escape(comment)
+    return f"<div class='comment'>{escaped}</div>"
